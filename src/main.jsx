9f86749397a3abb116660a0dbfa9e6478b453a92
astv06 <156257557+astv06@users.noreply.github.com>
2025-03-04 20:18:18 -0500

    pruebas de los botones

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,9 @@ import Layout from "./components/ui/layout/Layout";
 import Home from "./pages/home/Home";
 import Actividades from "./pages/actividades/Actividades";
 import Signup from "./pages/signup/Signup";
-// import Informacion from "./pages/informacion/Informacion";
-// import Foro from "./pages/foro/Foro";
-// import Actividades from "./pages/actividades/Actividades";
-// import Contacto from "./pages/contacto/Contacto";
+import Informacion from "./pages/informacion/Informacion";
+import Foro from "./pages/foro/Foro";
+import Contacto from "./pages/contacto/Contacto";
 
 export default function App() {
   return (
@@ -17,7 +16,9 @@ export default function App() {
         <Route element={<Layout/>}> 
           <Route index element={<Home/>} />  
           <Route path="actividades" element={<Actividades/>} />
-          {/* <Route path="contactanos" element={<Contacto />} /> */}
+          <Route path="foro" element={<Foro/>} />
+          <Route path="informacion" element={<Informacion/>} />
+          <Route path="contacto" element={<Contacto />} />
         </Route>
       </Routes>
     </BrowserRouter>
