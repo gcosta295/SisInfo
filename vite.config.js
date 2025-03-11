8642409e092d8d87605fa8e825221c241e57d367
gcosta295 <156736652+gcosta295@users.noreply.github.com>
2025-03-10 22:05:06 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build:{
+    target: "esnext", // or "es2019",
+  },
+  // other part of your config 
+  // plugins: [vue()],
+  // ...
 })
+
